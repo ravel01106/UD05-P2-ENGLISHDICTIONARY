3096c88747f980f0cf794fb4968a08900562492d
ravel01106 <ravello2002@gmail.com>
2023-05-09 19:54:23 +0100

    feat(Dictionary): addWord method is created correctly

--- src/net/duolingo/dictionary/Dictionary.java
+++ src/net/duolingo/dictionary/Dictionary.java
@@ -1,6 +1,7 @@
 package net.duolingo.dictionary;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -12,6 +13,42 @@ public class Dictionary {
 
     public Map<String, Set<String>> getDictionaryEnglish() {
         return dictionaryEnglish;
+    }
+
+    public void addWord(String word) {
+        String character = word.toLowerCase().substring(0, 1);
+        Set<String> arrayWords = new HashSet<>();
+        boolean hasSameKey = false;
+
+        if (dictionaryEnglish.size() == 0) {
+
+            arrayWords.add(word);
+            dictionaryEnglish.put(character, arrayWords);
+
+        } else {
+
+            Set<String> keys = dictionaryEnglish.keySet();
+
+            for (String key : keys) {
+                if (key.equals(character)) {
+                    hasSameKey = true;
+                }
+            }
+
+            if (!hasSameKey) {
+
+                arrayWords.add(word);
+                dictionaryEnglish.put(character, arrayWords);
+
+            } else {
+
+                arrayWords = dictionaryEnglish.get(character);
+                arrayWords.add(word);
+                dictionaryEnglish.put(character, arrayWords);
+
+            }
+        }
+
     }
 
 }
